f19fd7cf5ca6f0cb7e9c619d270c607884451cd1
lnapo94 <10464485@polimi.it>
2019-04-16 17:28:52 +0200

    Finished user routes

--- Controller/User/me.js
+++ Controller/User/me.js
@@ -10,7 +10,7 @@ exports.findUserByID = function (userID) {
             .then( (user) => {
 
                 if(!user)
-                    return reject(new Response(500, "No user exists with that id"));
+                    return reject(new Response(400, "No user exists with that id"));
 
                 // Create the user object to return
                 let returnUser = {
@@ -23,8 +23,51 @@ exports.findUserByID = function (userID) {
             })
             .catch( (err) => {
                 console.log(err);
-                reject(new Response(500, "No user exists with that id"));
+                reject(new Response(500, "Internal Server Error"));
             })
     })
+};
 
+exports.modifyUserByID = function (userID, name, surname, email, password) {
+    return new Promise( (resolve, reject) => {
+        User.findOne({
+            where: { id: userID}
+        })
+            .then( (user) => {
+                if(!user)
+                    return reject(new Response(400, "No user exists with that id"));
+
+                user.update({
+                    name: name,
+                    surname: surname,
+                    email: email,
+                    password: password
+                })
+                    .then( () => {
+                        let returnUser = {
+                            id: user.id,
+                            name: user.name,
+                            surname: user.surname,
+                            email: user.email
+                        };
+                        resolve(returnUser);
+                    })
+                    .catch( (err) => {
+                        let errorList = err.errors;
+                        console.log(errorList);
+
+                        if(errorList && errorList.length > 0)
+                            if(errorList[0].type === "unique violation")
+                                reject(new Response(409, 'Another user is already registered with the given email'));
+                            else
+                                reject(new Response(400, 'The given parameters are wrong or bad written'));
+                        else
+                            reject(new Response(500, 'Internal Server Error'));
+                    });
+            })
+            .catch( (err) => {
+                console.log(err);
+                reject(new Response(500, "Internal Server Error"))
+            });
+    });
 };
--- Routes/ApiRoutes/User/meRoute.js
+++ Routes/ApiRoutes/User/meRoute.js
@@ -3,12 +3,28 @@
 let express = require('express');
 let router = express.Router();
 
-const { findUserByID } = require('../../../Controller/User/me');
+const { findUserByID, modifyUserByID } = require('../../../Controller/User/me');
 const { checkAuthMiddleware } = require('../../../Middlewares/checkAuthenticationMiddleware');
 
 router.get('/', checkAuthMiddleware, function (req, res, next) {
 
     findUserByID(req.userID)
+        .then( (userObject) => {
+            res.status(200).send(userObject);
+        })
+        .catch( (err) => {
+            next(err);
+        })
+
+});
+
+router.put('/', checkAuthMiddleware, function (req, res, next) {
+    const email = req.body.email.toString();
+    const password = req.body.password.toString();
+    const name = req.body.name.toString();
+    const surname = req.body.surname.toString();
+
+    modifyUserByID(req.userID, name, surname, email, password)
         .then( (userObject) => {
             res.status(200).send(userObject);
         })
