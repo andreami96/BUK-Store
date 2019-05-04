c53e356c92df9b0b869e1aa0228ea2f153c76df0
Claudio <10453981@polimi.it>
2019-05-04 18:00:04 -0500

    Minor fixs on catalog page

--- public/assets/js/catalog.js
+++ public/assets/js/catalog.js
@@ -4,3 +4,45 @@ $(document).ready(function() {
     });
 });
 
+function mobileViewUpdate() {
+    var viewportWidth = $(window).width();
+
+    if (viewportWidth > 912 && viewportWidth < 1200) {
+        var element = document.getElementById("book-catalog");
+        element.className = element.className.replace(/\bcolumns-*\b/g, "");
+        element.classList.add("columns-3");
+    } else if (viewportWidth < 912) {
+        var element = document.getElementById("book-catalog");
+        element.className = element.className.replace(/\bcolumns-*\b/g, "");
+        element.classList.add("columns-2");
+    } else if (viewportWidth > 1200) {
+        var element = document.getElementById("book-catalog");
+        element.className = element.className.replace(/\bcolumns-*\b/g, "");
+        element.classList.add("columns-4");
+    }
+}
+
+$(document).ready(mobileViewUpdate());
+
+$(window).on('load, resize', function mobileViewUpdate() {
+    var viewportWidth = $(window).width();
+
+    if (viewportWidth > 912 && viewportWidth < 1200) {
+        var element = document.getElementById("book-catalog");
+        element.className = element.className.replace(/\bcolumns-*\b/g, "");
+        element.classList.add("columns-3");
+    }
+
+    else if (viewportWidth < 912) {
+        var element = document.getElementById("book-catalog");
+        element.className = element.className.replace(/\bcolumns-*\b/g, "");
+        element.classList.add("columns-2");
+    }
+
+    else if (viewportWidth > 1200) {
+        var element = document.getElementById("book-catalog");
+        element.className = element.className.replace(/\bcolumns-*\b/g, "");
+        element.classList.add("columns-4");
+    }
+});
+
