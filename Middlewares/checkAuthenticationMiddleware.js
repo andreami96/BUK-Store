d24e7d6642059ca5e9dff2282221a37c539cc3c6
lnapo94 <10464485@polimi.it>
2019-05-20 19:17:17 +0200

    Fix cookie bug, add a logout route, update the documentation and first implementation of the dynamic navbar

--- Middlewares/checkAuthenticationMiddleware.js
+++ Middlewares/checkAuthenticationMiddleware.js
@@ -9,7 +9,7 @@ const {check_session_token} = require('../Controller/User/authentication');
  * @returns {*}         If this middleware returns, it means that there is that X-API-KEY inside the application
  */
 exports.checkAuthMiddleware = function (req, res, next) {
-    let user_token = req.headers['x-api-key'];
+    let user_token = req.cookies['X-API-KEY'];
 
     if(!user_token) {
         let error = new Response(401, "The user in not authorized to access this endpoint");
