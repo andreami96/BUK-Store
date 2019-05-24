aab3f01f707bfddcaed06b177b71df57e2d67c2c
lnapo94 <10464485@polimi.it>
2019-05-24 23:27:01 +0200

    Refactoring navbar code

--- public/assets/js/navbar.js
+++ public/assets/js/navbar.js
@@ -3,7 +3,10 @@ $(document).ready(function () {
     $.ajax({
         url: "/api/v1/me",
         success: function (result, status, xhr) {
-            addLoggedNavbar();
+            if($(window).width() > 576)
+                addNavbar(true, true);
+            else
+                addNavbar(true, false);
             makeTransparentNavbar();
 
             if( !$("#navbarNoHover").length ){
@@ -27,7 +30,10 @@ $(document).ready(function () {
             else makeSolidNavbar();
         },
         error: function (xhr) {
-            addNormalNavbar();
+            if($(window).width() > 576)
+                addNavbar(false, true);
+            else
+                addNavbar(false, false);
             makeTransparentNavbar();
 
             if( !$("#navbarNoHover").length ){
@@ -59,83 +65,76 @@ $(document).ready(function () {
         if($('.navbar-toggler').is(':visible'))
             $('.navbar-toggler').click();
     });
-
 });
 
-function addNormalNavbar() {
-    $(".navbar").addClass("navbra-default navbar-expand-sm fixed-top")
-                .append("<button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarCollapse\" aria-controls=\"navbarCollapse\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
-                    "            <span class=\"navbar-toggler-icon\"></span>\n" +
-                    "        </button>\n" +
-                    "\n" +
-                    "        <div class=\"navbar-header mx-sm-auto\">\n" +
-                    "            <a id=\"brand-img\" class=\"navbar-brand\" href=\"/\">\n" +
-                    "                <img src=\"/assets/images/logo/logo-black.png\" height=\"30\" alt=\"BUK\">\n" +
-                    "            </a>\n" +
-                    "        </div>\n" +
-                    "\n" +
-                    "        <div class=\"empty-nav-space ml-sm-auto\"></div>\n" +
-                    "\n" +
-                    "        <div class=\"collapse navbar-collapse\" id=\"navbarCollapse\">\n" +
-                    "            <ul class=\"navbar-nav mr-auto\">\n" +
-                    "                <li class=\"nav-item dropdown\">\n" +
-                    "                    <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n" +
-                    "                        Catalogo\n" +
-                    "                    </a>\n" +
-                    "                    <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">\n" +
-                    "                        <a class=\"dropdown-item\" href=\"/catalogue/mainGenres.html\">Generi Principali</a>\n" +
-                    "                        <a class=\"dropdown-item\" href=\"#\">Temi</a>\n" +
-                    "                        <div class=\"dropdown-divider\"></div>\n" +
-                    "                        <a class=\"dropdown-item\" href=\"#\">Something to divide from the other</a>\n" +
-                    "                    </div>\n" +
-                    "                </li>\n" +
-                    "                <li class=\"nav-item\">\n" +
-                    "                    <a class=\"nav-link\" href=\"#\">Eventi</a>\n" +
-                    "                </li>\n" +
-                    "                <li class=\"nav-item\">\n" +
-                    "                    <a class=\"nav-link\" href=\"#\">Autori</a>\n" +
-                    "                </li>\n" +
-                    "            </ul>\n" +
-                    "            <ul class=\"navbar-nav\" id=\"right-nav\">\n" +
-                    "                <li class=\"nav-item\">\n" +
-                    "                    <a class=\"nav-link\" href=\"/signup.html\">Registrati</a>\n" +
-                    "                </li>\n" +
-                    "                <li class=\"nav-item\">\n" +
-                    "                    <a class=\"nav-link\" href=\"/login.html\">Login</a>\n" +
-                    "                </li>\n" +
-                    "            </ul>\n" +
-                    "        </div>\n");
+function addNavbar(isLogged, dropdown) {
+    $(".navbar").addClass("navbar-default navbar-expand-sm fixed-top");
+    createToggler();
+    createHeader(isLogged);
+    if(dropdown)
+        createDropdownMenus(isLogged);
+    else
+        createUnPackedMenus(isLogged);
 }
 
-function addLoggedNavbar() {
-    $('.navbar')
-        .addClass("navbar-expand-sm fixed-top")
+function createToggler() {
+    $(".navbar")
         .append("<button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarCollapse\" aria-controls=\"navbarCollapse\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\n" +
-            "            <span class=\"navbar-toggler-icon\"></span>\n" +
-            "        </button>\n" +
-            "\n" +
-            "        <div class=\"navbar-header mx-sm-auto\">\n" +
-            "            <a id=\"brand-img\" class=\"navbar-brand\" href=\"/\">\n" +
-            "                <img src=\"/assets/images/logo/logo-black.png\" height=\"30\" alt=\"BUK\">\n" +
-            "            </a>\n" +
-            "        </div>\n" +
-            "\n" +
-            "        <div class=\"nav-item order-md-12 order-sm-1\">\n" +
-            "            <a><span class=\"badge badge-primary\">1</span></a>\n" +
-            "        </div>\n" +
-            "\n" +
-            "        <div class=\"collapse navbar-collapse\" id=\"navbarCollapse\">\n" +
+                "   <span class=\"navbar-toggler-icon\"></span>\n" +
+                "</button>\n");
+}
+
+function retrieveBadge() {
+    return  "<div class=\"nav-item order-md-12 order-sm-1\">\n" +
+            "   <a><span class=\"badge badge-primary\">1</span></a>\n" +
+            "</div>\n"
+}
+
+function retrieveRightMenu(isLogged) {
+
+    let notLogged = "<ul class=\"navbar-nav\" id=\"right-nav\">\n" +
+                    "   <li class=\"nav-item\">\n" +
+                    "      <a class=\"nav-link\" href=\"/signup.html\">Registrati</a>\n" +
+                    "   </li>\n" +
+                    "   <li class=\"nav-item\">\n" +
+                    "      <a class=\"nav-link\" href=\"/login.html\">Login</a>\n" +
+                    "   </li>\n" +
+                    "</ul>";
+
+    let logged =    "<ul class=\"navbar-nav\" id=\"right-nav\">\n" +
+                    "   <li class=\"nav-item\">\n" +
+                    "      <a class=\"nav-link\" href=\"#\">Area Personale</a>\n" +
+                    "   </li>\n" +
+                    "   <li class=\"nav-item\">\n" +
+                    "      <a class=\"nav-link\" href=\"#\" onclick=\"logout()\">Logout</a>\n" +
+                    "   </li>\n" +
+                    "</ul>\n";
+
+    return (isLogged) ? logged : notLogged;
+}
+
+function createHeader(isLogged) {
+    $(".navbar")
+        .append("<div class=\"navbar-header mx-sm-auto\">\n" +
+                "   <a id=\"brand-img\" class=\"navbar-brand\" href=\"/\">\n" +
+                "      <img src=\"/assets/images/logo/logo-black.png\" height=\"30\" alt=\"BUK\">\n" +
+                "   </a>\n" +
+                "</div>\n" +
+                ((isLogged) ? retrieveBadge() : "<div class=\"empty-nav-space ml-sm-auto\"></div>\n"));
+}
+
+function createUnPackedMenus(isLogged) {
+    $(".navbar")
+        .append("<div class=\"collapse navbar-collapse\" id=\"navbarCollapse\">\n" +
             "            <ul class=\"navbar-nav mr-auto\">\n" +
-            "                <li class=\"nav-item dropdown\">\n" +
-            "                    <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n" +
-            "                        Catalogo\n" +
-            "                    </a>\n" +
-            "                    <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">\n" +
-            "                        <a class=\"dropdown-item\" href=\"/catalogue/mainGenres.html\">Generi Principali</a>\n" +
-            "                        <a class=\"dropdown-item\" href=\"#\">Temi</a>\n" +
-            "                        <div class=\"dropdown-divider\"></div>\n" +
-            "                        <a class=\"dropdown-item\" href=\"#\">Something to divide from the other</a>\n" +
-            "                    </div>\n" +
+                            "<li class=\"nav-item\">\n" +
+            "                    <a class=\"nav-link\" href=\"/catalogue/1\">Catalogo Libri</a>\n" +
+            "                </li>\n" +
+            "                <li class=\"nav-item\">\n" +
+            "                    <a class=\"nav-link\" href=\"/catalogue/mainGenres.html\">Generi Principali</a>\n" +
+            "                </li>\n" +
+            "                <li class=\"nav-item\">\n" +
+            "                    <a class=\"nav-link\" href=\"#\">Temi</a>\n" +
             "                </li>\n" +
             "                <li class=\"nav-item\">\n" +
             "                    <a class=\"nav-link\" href=\"#\">Eventi</a>\n" +
@@ -143,16 +142,33 @@ function addLoggedNavbar() {
             "                <li class=\"nav-item\">\n" +
             "                    <a class=\"nav-link\" href=\"#\">Autori</a>\n" +
             "                </li>\n" +
-            "            </ul>\n" +
-            "            <ul class=\"navbar-nav\" id=\"right-nav\">\n" +
-            "            <li class=\"nav-item\">\n" +
-            "                    <a class=\"nav-link\" href=\"#\">Area Personale</a>\n" +
-            "                </li>\n" +
-            "                <li class=\"nav-item\">\n" +
-            "                    <a class=\"nav-link\" href=\"#\" onclick=\"logout()\">Logout</a>\n" +
-            "                </li>\n" +
-            "            </ul>\n" +
-            "        </div>\n")
+            "            </ul>\n" + retrieveRightMenu(isLogged) +
+            "</div>");
+}
+
+function createDropdownMenus(isLogged) {
+    $(".navbar")
+        .append("<div class=\"collapse navbar-collapse\" id=\"navbarCollapse\">\n" +
+                "            <ul class=\"navbar-nav mr-auto\">\n" +
+                "                <li class=\"nav-item dropdown\">\n" +
+                "                    <a class=\"nav-link dropdown-toggle\" href=\"#\" id=\"navbarDropdown\" role=\"button\" data-toggle=\"dropdown\" aria-haspopup=\"true\" aria-expanded=\"false\">\n" +
+                "                        Sfoglia\n" +
+                "                    </a>\n" +
+                "                    <div class=\"dropdown-menu\" aria-labelledby=\"navbarDropdown\">\n" +
+                "                        <a class=\"dropdown-item\" href=\"/catalogue/1\">Catalogo Libri</a>\n" +
+                "                        <div class=\"dropdown-divider\"></div>\n" +
+                "                        <a class=\"dropdown-item\" href=\"/catalogue/mainGenres.html\">Generi Principali</a>\n" +
+                "                        <a class=\"dropdown-item\" href=\"#\">Temi</a>\n" +
+                "                    </div>\n" +
+                "                </li>\n" +
+                "                <li class=\"nav-item\">\n" +
+                "                    <a class=\"nav-link\" href=\"#\">Eventi</a>\n" +
+                "                </li>\n" +
+                "                <li class=\"nav-item\">\n" +
+                "                    <a class=\"nav-link\" href=\"#\">Autori</a>\n" +
+                "                </li>\n" +
+                "            </ul>\n" + retrieveRightMenu(isLogged) +
+                " </div>");
 }
 
 function logout() {
